0d9df72ebb183451ca07ec8dfc638cca75684b80
Faysal Ahmed <faysal.ahmed833@gmail.com>
2020-04-08 15:45:57 +0600

    implemented custom reducer

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 function customMap(arr,inc) {
   var [elm, ...remains] = arr;
-
   if (elm === undefined) {
     return [];
   } else {
@@ -10,9 +9,19 @@ function customMap(arr,inc) {
   }
 }
 
+function customReduce(arr) {
+  var [elm, ...remains] = arr;
+  if (elm === undefined) {
+    return 0;
+  } else {
+    return elm + customReduce(remains);
+
+  }
+}
+
 // example 
 
 arr = [2, 3, 4];
-
-console.log(customMap(arr, 2));
+console.log(customMap(arr,2))
+console.log(customReduce(arr));
 // should return [4,5,6]
